23ffd041b37af53bee987033600117fcb59f5409
oren <orenbm11@gmail.com>
2023-05-04 06:49:38 -0400

    add unversioned files

---
+++ projects/poll-service/src/test/java/com/pollvite/pollservice/PollServiceImplApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.pollvite.pollservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PollServiceImplApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
