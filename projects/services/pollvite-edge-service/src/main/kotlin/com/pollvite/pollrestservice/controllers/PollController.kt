7b0b44715f31b61211589c410d1b21dc91883de0
oren <orenbm11@gmail.com>
2023-05-12 01:25:08 -0400

    Switch to reactive grpc

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/controllers/PollController.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/controllers/PollController.kt
@@ -3,7 +3,7 @@ package com.pollvite.pollrestservice.controllers
 import com.pollvite.pollrestservice.dtos.PollChanCreateDto
 import com.pollvite.pollrestservice.dtos.PollChanEditDto
 import com.pollvite.pollrestservice.dtos.PollChanReadDto
-import com.pollvite.pollrestservice.services.PollChanRpcService
+import com.pollvite.pollrestservice.services.PollChanClientService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
@@ -11,25 +11,25 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("api/poll")
-class PollController(@Autowired val pollChanRpcService: PollChanRpcService) {
+class PollController(@Autowired val pollChanClientService: PollChanClientService) {
 
     @GetMapping("/channel/{id}")
     fun getPollCHanById(@PathVariable id: String): Mono<PollChanReadDto>? {
-        return pollChanRpcService.getPollChannelById(id)
+        return pollChanClientService.getPollChannelById(id)
     }
 
     @PostMapping("/channel")
     fun createPollChannel(@RequestBody @Valid dtoSrc: Mono<PollChanCreateDto>): Mono<PollChanReadDto> {
-        return pollChanRpcService.createPollChannel(dtoSrc)
+        return pollChanClientService.createPollChannel(dtoSrc)
     }
 
     @PutMapping("/channel")
     fun editPollChannel(@RequestBody @Valid dtoSrc: Mono<PollChanEditDto>): Mono<PollChanReadDto> {
-        return pollChanRpcService.editPollChannel(dtoSrc)
+        return pollChanClientService.editPollChannel(dtoSrc)
     }
 
     @DeleteMapping("/channel/{id}")
     fun deletePollChannel(@PathVariable id: String): Mono<PollChanReadDto> {
-        return pollChanRpcService.deletePollChannel(id)
+        return pollChanClientService.deletePollChannel(id)
     }
 }
--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/services/PollChanClientService.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/services/PollChanClientService.kt
@@ -1,17 +1,16 @@
 package com.pollvite.pollrestservice.services
 
-import com.pollvite.grpc.poll.PollChanServiceGrpc.PollChanServiceStub
+import com.pollvite.grpc.poll.ReactorPollChanServiceGrpc.ReactorPollChanServiceStub
 import com.pollvite.pollrestservice.dtos.IdDto
 import com.pollvite.pollrestservice.dtos.PollChanCreateDto
 import com.pollvite.pollrestservice.dtos.PollChanEditDto
 import com.pollvite.pollrestservice.dtos.PollChanReadDto
-import com.pollvite.pollrestservice.reactive.ReactiveGrpc
 import net.devh.boot.grpc.client.inject.GrpcClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
-interface PollChanRpcService {
+interface PollChanClientService {
     fun getPollChannelById(id: String): Mono<PollChanReadDto>
     fun createPollChannel(dtoSrc: Mono<PollChanCreateDto>): Mono<PollChanReadDto>
     fun editPollChannel(dtoSrc: Mono<PollChanEditDto>): Mono<PollChanReadDto>
@@ -19,32 +18,32 @@ interface PollChanRpcService {
 }
 
 @Service
-class PollChanRpcServiceImpl(
-    @Autowired @GrpcClient("pollService") private val pollChanServiceClient: PollChanServiceStub? = null)
-    : PollChanRpcService {
+class PollChanClientServiceImpl(
+    @Autowired @GrpcClient("pollService") private val pollChanServiceStub: ReactorPollChanServiceStub? = null)
+    : PollChanClientService {
 
     override fun getPollChannelById(id: String): Mono<PollChanReadDto> {
         val idPb = IdDto(id).toPb()
-        return ReactiveGrpc.callMono(pollChanServiceClient!!::getPollChanById, idPb)
-            .map(PollChanReadDto::fromPb)
+        return pollChanServiceStub?.getPollChanById(idPb)
+            ?.map(PollChanReadDto::fromPb) ?: Mono.empty()
     }
 
     override fun createPollChannel(dtoSrc: Mono<PollChanCreateDto>): Mono<PollChanReadDto> {
         return dtoSrc.flatMap { dto ->
-            ReactiveGrpc.callMono(pollChanServiceClient!!::createPollChan, dto.toPb())
+            pollChanServiceStub?.createPollChan(dto.toPb()) ?: Mono.empty()
         }.map(PollChanReadDto::fromPb)
     }
 
     override fun editPollChannel(dtoSrc: Mono<PollChanEditDto>): Mono<PollChanReadDto> {
         return dtoSrc.flatMap { dto ->
-            ReactiveGrpc.callMono(pollChanServiceClient!!::editPollChan, dto.toPb())
+            pollChanServiceStub?.editPollChan(dto.toPb()) ?: Mono.empty()
         }.map(PollChanReadDto::fromPb)
     }
 
     override fun deletePollChannel(id: String): Mono<PollChanReadDto> {
         val idPb = IdDto(id).toPb()
-        return ReactiveGrpc.callMono(pollChanServiceClient!!::deletePollChan, idPb)
-            .map(PollChanReadDto::fromPb)
+        return pollChanServiceStub?.deletePollChan(idPb)
+            ?.map(PollChanReadDto::fromPb) ?: Mono.empty()
     }
 
 }
