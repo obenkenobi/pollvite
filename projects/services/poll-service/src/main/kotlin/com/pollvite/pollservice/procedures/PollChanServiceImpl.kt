bdf3b28c3d5dbd6e33a97985b6869e008d141618
oren <orenbm11@gmail.com>
2023-05-12 01:41:54 -0400

    Use kotlin class apis for building data

--- projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/procedures/PollChanServiceImpl.kt
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/procedures/PollChanServiceImpl.kt
@@ -9,47 +9,45 @@ import net.devh.boot.grpc.server.service.GrpcService
 private class PollChanServiceImpl() : PollChanServiceGrpc.PollChanServiceImplBase() {
     @Override
     override fun getPollChanById(request: IdPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value: PollChanReadPb = PollChanReadPb.newBuilder()
-            .setId(request?.value)
-            .setCore(
-                PollChanCorePb.newBuilder()
-                .setOwner("Nero")
-                .setDescription("food based polls")
-                .setTitle("Food Polls")
-                .build())
-            .build()
+        val value: PollChanReadPb = PollChanReadPb.newBuilder().also {
+            it.id = request?.value
+            it.core = PollChanCorePb.newBuilder().also { core ->
+                core.owner = "Nero"
+                core.description = "food based polls"
+                core.title = "Food Polls"
+            }.build()
+        }.build()
         resObserver?.onNext(value)
         resObserver?.onCompleted();
     }
 
     override fun createPollChan(request: PollChanCreatePb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value = PollChanReadPb.newBuilder()
-            .setCore(request?.core)
-            .setId("someId")
-            .build()
+        val value = PollChanReadPb.newBuilder().apply {
+            id = "someId"
+            core = request?.core
+        }.build()
         resObserver?.onNext(value)
         resObserver?.onCompleted();
     }
 
     override fun editPollChan(request: PollChanEditPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value = PollChanReadPb.newBuilder()
-            .setCore(request?.core)
-            .setId("someId")
-            .build()
+        val value = PollChanReadPb.newBuilder().apply {
+            core = request?.core
+            id = request?.id
+        }.build()
         resObserver?.onNext(value)
         resObserver?.onCompleted();
     }
 
     override fun deletePollChan(request: IdPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value: PollChanReadPb = PollChanReadPb.newBuilder()
-            .setId(request?.value)
-            .setCore(
-                PollChanCorePb.newBuilder()
-                .setOwner("Nero")
-                .setDescription("food based polls")
-                .setTitle("Food Polls")
-                .build())
-            .build()
+        val value: PollChanReadPb = PollChanReadPb.newBuilder().also {
+            it.id = request?.value
+            it.core = PollChanCorePb.newBuilder().also { core ->
+                core.owner = "Nero"
+                core.description = "food based polls"
+                core.title = "Food Polls"
+            }.build()
+        }.build()
         resObserver?.onNext(value)
         resObserver?.onCompleted();
     }
