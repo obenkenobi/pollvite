4177f68167ee661445206234f7faae27d55ae093
oren <orenbm11@gmail.com>
2023-05-13 04:29:59 -0400

    Switch grpc server to reactive

--- projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/procedures/PollChanServiceImpl.kt
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/procedures/PollChanServiceImpl.kt
@@ -2,53 +2,52 @@ package com.pollvite.pollservice.procedures
 
 import com.pollvite.grpc.poll.*
 import com.pollvite.grpc.shared.IdPb
-import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
+import reactor.core.publisher.Mono
 
 @GrpcService
-private class PollChanServiceImpl() : PollChanServiceGrpc.PollChanServiceImplBase() {
+private class PollChanServiceImpl() : ReactorPollChanServiceGrpc.PollChanServiceImplBase() {
     @Override
-    override fun getPollChanById(request: IdPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value: PollChanReadPb = PollChanReadPb.newBuilder().also {
-            it.id = request?.value
+    override fun getPollChanById(requst: Mono<IdPb>) : Mono<PollChanReadPb> {
+        return requst.map { req ->
+            PollChanReadPb.newBuilder().also {
+                it.id = req?.value
+                it.core = PollChanCorePb.newBuilder().also { core ->
+                    core.owner = "Nero"
+                    core.description = "food based polls"
+                    core.title = "Food Polls"
+                }.build()
+            }.build()
+        }
+    }
+
+    override fun createPollChan(request: Mono<PollChanCreatePb>) : Mono<PollChanReadPb> {
+        return request.map { req ->
+            PollChanReadPb.newBuilder().apply {
+                id = "someId"
+                core = req?.core
+            }.build()
+        }
+    }
+
+    override fun editPollChan(request: Mono<PollChanEditPb>) : Mono<PollChanReadPb> {
+        return request.map { req ->
+            PollChanReadPb.newBuilder().apply {
+                id = req?.id
+                core = req?.core
+            }.build()
+        }
+    }
+
+    override fun getPollChanById(requst:IdPb) : Mono<PollChanReadPb> {
+        val value = PollChanReadPb.newBuilder().also {
+            it.id = requst?.value
             it.core = PollChanCorePb.newBuilder().also { core ->
                 core.owner = "Nero"
                 core.description = "food based polls"
                 core.title = "Food Polls"
             }.build()
         }.build()
-        resObserver?.onNext(value)
-        resObserver?.onCompleted();
-    }
-
-    override fun createPollChan(request: PollChanCreatePb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value = PollChanReadPb.newBuilder().apply {
-            id = "someId"
-            core = request?.core
-        }.build()
-        resObserver?.onNext(value)
-        resObserver?.onCompleted();
-    }
-
-    override fun editPollChan(request: PollChanEditPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value = PollChanReadPb.newBuilder().apply {
-            core = request?.core
-            id = request?.id
-        }.build()
-        resObserver?.onNext(value)
-        resObserver?.onCompleted();
-    }
-
-    override fun deletePollChan(request: IdPb?, resObserver: StreamObserver<PollChanReadPb>?) {
-        val value: PollChanReadPb = PollChanReadPb.newBuilder().also {
-            it.id = request?.value
-            it.core = PollChanCorePb.newBuilder().also { core ->
-                core.owner = "Nero"
-                core.description = "food based polls"
-                core.title = "Food Polls"
-            }.build()
-        }.build()
-        resObserver?.onNext(value)
-        resObserver?.onCompleted();
+        return Mono.just(value)
     }
 }
