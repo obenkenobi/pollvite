2e34aa2037e65578c97722243c10d09821aef541
oren <orenbm11@gmail.com>
2023-05-13 04:10:10 -0400

    Add timestamp and audit schema protos

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/dtos/commonDtos.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/pollrestservice/dtos/commonDtos.kt
@@ -1,10 +1,11 @@
 package com.pollvite.pollrestservice.dtos
 
+import com.pollvite.grpc.shared.AuditPb
 import com.pollvite.grpc.shared.IdPb
+import com.pollvite.grpc.shared.TimestampsPb
 import javax.validation.constraints.NotNull
 
 data class IdDto(@field:NotNull val value: String?) {
-    
     companion object {
       fun fromPb(idPb: IdPb) : IdDto {
           return IdDto(value = idPb.value)
@@ -14,6 +15,36 @@ data class IdDto(@field:NotNull val value: String?) {
     fun toPb(): IdPb {
         return IdPb.newBuilder().also {
             it.value = value
+        }.build()
+    }
+}
+
+data class TimestampsDto(val createdAt: Long, val updatedAt: Long) {
+    companion object {
+        fun fromPb(timestampsPb: TimestampsPb) : TimestampsDto {
+            return TimestampsDto(createdAt = timestampsPb.createdAt, updatedAt = timestampsPb.updatedAt)
+        }
+    }
+
+    fun toPb(): TimestampsPb {
+        return TimestampsPb.newBuilder().also {
+            it.createdAt = createdAt
+            it.updatedAt = updatedAt
+        }.build()
+    }
+}
+
+data class AuditDto(val createdBy: String, val updatedBy: String) {
+    companion object {
+        fun fromPb(pb: AuditPb) : AuditDto {
+            return AuditDto(createdBy = pb.createdBy, updatedBy = pb.updatedBy)
+        }
+    }
+
+    fun toPb(): AuditPb {
+        return AuditPb.newBuilder().also {
+            it.createdBy = createdBy
+            it.createdBy = createdBy
         }.build()
     }
 }
