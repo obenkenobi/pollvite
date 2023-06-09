84a0c3ebff29d3ba12c0d1ebd618ca14dc91ca83
oren <orenbm11@gmail.com>
2023-06-09 04:57:41 -0400

    Clean up html render

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/ViewController.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/ViewController.kt
@@ -1,23 +1,18 @@
 package com.pollvite.polledgeservice.controllers
 
-import com.pollvite.polledgeservice.configuration.FirebasePropsConfig
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
-import reactor.core.publisher.Mono
 
 
 @Controller
-class ViewController(@Autowired val firebasePropsConfig: FirebasePropsConfig) {
+class ViewController {
 
     @Profile("dev")
     @RequestMapping("/firebase")
-    fun index(model: Model): Mono<String> {
-        return Mono.fromSupplier {
-            return@fromSupplier "firebase"
-        }
+    fun index(model: Model): String {
+        return "firebase"
     }
 
 }
