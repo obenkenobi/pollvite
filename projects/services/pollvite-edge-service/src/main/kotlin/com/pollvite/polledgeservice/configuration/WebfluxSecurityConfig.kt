2301974968c5e922b83474646551e217ce2651c9
oren <orenbm11@gmail.com>
2023-06-09 19:45:47 -0400

    Convert blocking to nonblocking call session create

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/configuration/WebfluxSecurityConfig.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/configuration/WebfluxSecurityConfig.kt
@@ -1,40 +1,71 @@
 package com.pollvite.polledgeservice.configuration
-
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer.withDefaults
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.web.server.SecurityWebFilterChain
-
-
-@Configuration
-@EnableWebFluxSecurity
-class WebfluxSecurityConfig {
-    @Bean
-    fun userDetailsService(): MapReactiveUserDetailsService? {
-        val user: UserDetails = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("user")
-            .roles("USER")
-            .build()
-        return MapReactiveUserDetailsService(user)
-    }
-
-    @Bean
-    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
-        http
-            .authorizeExchange { exchanges: AuthorizeExchangeSpec ->
-                exchanges
-                    .anyExchange().permitAll()
-            }
-            .csrf { it.disable() }
-            .httpBasic(withDefaults())
-            .formLogin(withDefaults())
-        return http.build()
-    }
-}
+//
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.auth.FirebaseAuthException
+//import com.google.firebase.auth.FirebaseToken
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.autoconfigure.security.SecurityProperties
+//import org.springframework.http.server.reactive.ServerHttpRequest
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+//import org.springframework.security.core.context.SecurityContextHolder
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+//import org.springframework.stereotype.Component
+//import org.springframework.web.server.ServerWebExchange
+//import org.springframework.web.server.WebFilter
+//import org.springframework.web.server.WebFilterChain
+//import reactor.core.publisher.Mono
+//import java.security.Principal
+//
+//
+//class User(
+//    val uuid: String,
+//    val issuer: String,
+//    val isEmailVerified: Boolean
+//) : Principal {
+//    override fun getName(): String {
+//        return uuid
+//    }
+//}
+//
+//class Credentials(val type: Type, decodedToken: FirebaseToken, session: String) {
+//    enum class Type {ID_TOKEN, SESSION}
+//}
+//
+//@Component
+//class SecurityFilter (
+//    @Autowired securityService: SecurityService,
+//    @Autowired restSecProps: SecurityProperties,
+//    @Autowired cookieUtils: cookieUtils,
+//    @Autowired securityProps: SecurityProperties
+//): WebFilter {
+//
+//    override fun filter(
+//        serverWebExchange: ServerWebExchange,
+//        webFilterChain: WebFilterChain
+//    ): Mono<Void> {
+//        verifyToken(serverWebExchange.request)
+//        return webFilterChain.filter(serverWebExchange)
+//    }
+//
+//    private fun verifyToken(request: ServerHttpRequest) {
+//        val session = request.cookies["X-Auth"]?.firstOrNull()?.value ?: return
+//        val type = Credentials.Type.SESSION
+//        val decodedToken: FirebaseToken = try {
+//            FirebaseAuth.getInstance().verifySessionCookie(session, true)
+//        } catch (_: FirebaseAuthException) {
+//            null
+//        } ?: return
+//
+//        val user = firebaseTokenToUserPrinciple(decodedToken)
+//        val authentication = UsernamePasswordAuthenticationToken(
+//            user,
+//            Credentials(type, decodedToken, session), null
+//        )
+//        SecurityContextHolder.getContext().authentication = authentication
+//    }
+//
+//    private fun firebaseTokenToUserPrinciple(decodedToken: FirebaseToken): User = User(
+//        uuid = decodedToken.uid,
+//        issuer = decodedToken.issuer,
+//        isEmailVerified = decodedToken.isEmailVerified)
+//}
--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/services/FirebaseService.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/services/FirebaseService.kt
@@ -7,10 +7,12 @@ import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.SessionCookieOptions
 import com.pollvite.polledgeservice.configuration.FirebasePropsConfig
 import com.pollvite.polledgeservice.dtos.LoginDto
+import com.pollvite.polledgeservice.utils.toMono
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.io.File
+
 
 interface FirebaseService {
     fun getWebConfig(): Map<String, Any>
@@ -33,10 +35,10 @@ class FirebaseServiceImpl(@Autowired private val firebasePropsConfig: FirebasePr
         return fbWebConfig;
     }
 
-    override fun createSessionJwt(loginDto: Mono<LoginDto>): Mono<String> = loginDto.map {
+    override fun createSessionJwt(loginDto: Mono<LoginDto>): Mono<String> = loginDto.flatMap {
         val auth = FirebaseAuth.getInstance()
         val sessOpt = SessionCookieOptions.builder().setExpiresIn(3600000).build()
-        val jwt = auth.createSessionCookie(it.token, sessOpt)
-        jwt
+        val jwtFuture = auth.createSessionCookieAsync(it.token, sessOpt)
+        jwtFuture.toMono()
     }.onErrorResume({ it is FirebaseAuthException}, { Mono.empty() })
 }
