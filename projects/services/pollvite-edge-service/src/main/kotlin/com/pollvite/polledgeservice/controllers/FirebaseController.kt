066079c7d6e2bc76fdd2b52ecabefb16f0154918
oren <orenbm11@gmail.com>
2023-06-09 05:56:43 -0400

    Clean imports

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/FirebaseController.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/FirebaseController.kt
@@ -1,11 +1,8 @@
 package com.pollvite.polledgeservice.controllers
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.pollvite.polledgeservice.configuration.FirebasePropsConfig
 import com.pollvite.polledgeservice.services.FirebaseService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import java.io.File
 
 @RestController
 @RequestMapping("fb")
