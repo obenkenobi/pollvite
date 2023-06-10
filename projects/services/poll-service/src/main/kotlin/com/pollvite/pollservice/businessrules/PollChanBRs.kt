dee9d8f1f8a204d355516d715d0ceacc8c3f488a
oren <orenbm11@gmail.com>
2023-06-10 00:55:54 -0400

    Split Brs into interface and implementation

--- projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/businessrules/PollChanBRs.kt
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/businessrules/PollChanBRs.kt
@@ -4,9 +4,13 @@ import com.pollvite.grpc.poll.PollChanAccessPb
 import com.pollvite.pollservice.models.PollChan
 import org.springframework.stereotype.Component
 
+interface PollChanBRs {
+    fun validatePollChanDelete(accessPb: PollChanAccessPb, existing: PollChan): List<String>
+}
+
 @Component
-class PollChanBRs {
-    fun validatePollChanDelete(accessPb: PollChanAccessPb, existing: PollChan): List<String> {
+class PollChanBRsImpl: PollChanBRs {
+    override fun validatePollChanDelete(accessPb: PollChanAccessPb, existing: PollChan): List<String> {
         val violations = ArrayList<String>()
         if (accessPb.userId != existing.core.owner) {
             violations.add("Not a poll owner")
--- projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/services/BusinessRulesService.kt
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/services/BusinessRulesService.kt
@@ -1,15 +1,17 @@
 package com.pollvite.pollservice.services
 
-import com.pollvite.pollservice.businessrules.PollChanBRs
 import com.pollvite.shared.errors.AppException
 import com.pollvite.shared.errors.ErrorStatus
 import org.apache.logging.log4j.util.Strings
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+interface BusinessRulesService {
+    fun processBrResults(violations: List<String>): AppException?
+}
+
 @Service
-class BusinessRulesService(@Autowired private val pollChanBrs: PollChanBRs) {
-    fun processBrResults(violations: List<String>): AppException? {
+class BusinessRulesServiceImpl : BusinessRulesService {
+    override fun processBrResults(violations: List<String>): AppException? {
         if (violations.isEmpty())  {
             return null
         }
