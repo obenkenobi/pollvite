bb8412b892bb2347b679389da5fadf099e4ee942
oren <orenbm11@gmail.com>
2023-06-12 01:33:45 -0400

    Add PollChanRepositoryCustomImpl

--- projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/repositories/PollChanRepository.kt
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/repositories/PollChanRepository.kt
@@ -3,4 +3,4 @@ package com.pollvite.pollservice.repositories
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import com.pollvite.pollservice.models.PollChan
 
-interface PollChanRepository : ReactiveMongoRepository<PollChan, String>
+interface PollChanRepository : ReactiveMongoRepository<PollChan, String>, PollChanRepositoryCustom
---
+++ projects/services/poll-service/src/main/kotlin/com/pollvite/pollservice/repositories/PollChanRepositoryCustomImpl.kt
@@ -0,0 +1,13 @@
+package com.pollvite.pollservice.repositories
+
+import com.pollvite.pollservice.models.PollChan
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+
+class PollChanRepositoryCustomImpl(@Autowired mongoTemplate: ReactiveMongoTemplate): PollChanRepositoryCustom {
+    override fun getPageWithFilter(pageable: Pageable, owner: String?, namePattern: String?): Page<PollChan> {
+        TODO("Not yet implemented")
+    }
+}
