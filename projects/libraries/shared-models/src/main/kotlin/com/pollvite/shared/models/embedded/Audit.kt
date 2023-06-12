faa231fab584c3d018df7e40c97c791f04d2aff1
oren <orenbm11@gmail.com>
2023-06-11 23:56:26 -0400

    Update embedded shared model Audit doc with params

--- projects/libraries/shared-models/src/main/kotlin/com/pollvite/shared/models/embedded/Audit.kt
+++ projects/libraries/shared-models/src/main/kotlin/com/pollvite/shared/models/embedded/Audit.kt
@@ -2,6 +2,8 @@ package com.pollvite.shared.models.embedded
 
 /**
  * Embedded immutable POJO containing audit fields for a data persistence model.
+ * @param createdBy creator of the data persistence model instance.
+ * @param updatedBy editor of the data persistence model instance.
  * */
 data class Audit(
     val createdBy: String,
