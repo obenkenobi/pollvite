07926205c64c9c3b4ae346baef6b06ff6031e403
oren <orenbm11@gmail.com>
2023-06-12 00:05:20 -0400

    Edit Timestamps doc.

--- projects/libraries/shared-models/src/main/kotlin/com/pollvite/shared/models/embedded/Timestamps.kt
+++ projects/libraries/shared-models/src/main/kotlin/com/pollvite/shared/models/embedded/Timestamps.kt
@@ -4,14 +4,17 @@ import java.time.Instant
 
 /**
  * Embedded immutable POJO containing timestamp fields for a data persistence model.
- * @param createdAt When the data persistence model instance was created.
- * @param updatedAt When the data persistence model instance was updated.
+ * @param createdAt When the data persistence model instance was created in UTC time measured in milliseconds.
+ * @param updatedAt When the data persistence model instance was updated in UTC time measured in milliseconds.
  * */
 data class Timestamps(
     val createdAt: Long,
     val updatedAt: Long,
 ) {
     companion object {
+        /**
+         * @return A [Timestamps] instance where [createdAt] and the [updatedAt] are the current timestamp.
+         * */
         fun create(): Timestamps {
             val current = Instant.now().toEpochMilli()
             return Timestamps(
