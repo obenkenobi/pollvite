7206dd5413d24711eadc6104e99db3d772c3e238
oren <orenbm11@gmail.com>
2023-06-11 23:35:20 -0400

    Add links to elements in BusinessRuleResult doc

--- projects/libraries/springboot-businessrules-manager/src/main/kotlin/com/pollvite/springboot/businessrules/BusinessRuleResult.kt
+++ projects/libraries/springboot-businessrules-manager/src/main/kotlin/com/pollvite/springboot/businessrules/BusinessRuleResult.kt
@@ -9,14 +9,14 @@ import com.pollvite.shared.errors.AppException
 class BusinessRuleResult private constructor(val appException: AppException? = null) {
     companion object {
         /**
-         * Creates a passing BusinessRuleResult. Use this method if your business rules passed.
-         * @return A passing BusinessRuleResult.
+         * Creates a passing [BusinessRuleResult]. Use this method if your business rules passed.
+         * @return A passing [BusinessRuleResult].
          * */
         fun pass(): BusinessRuleResult = BusinessRuleResult()
         /**
-         * Creates a failing BusinessRuleResult. Use this method if your business rules failed.
-         * @param ex An exception describing why your business rules failed.
-         * @return A failing BusinessRuleResult.
+         * Creates a failing [BusinessRuleResult]. Use this method if your business rules failed.
+         * @param ex An [AppException] instance describing why your business rules failed.
+         * @return A failing [BusinessRuleResult].
          * */
         fun fail(ex: AppException): BusinessRuleResult = BusinessRuleResult(appException = ex)
     }
@@ -32,11 +32,11 @@ class BusinessRuleResult private constructor(val appException: AppException? = n
      * Wrapper method to handle if your business rules have passed or failed.
      * @param V The type to be returned when handling if your business rules passed or failed.
      * @param passBlock If your business rules passed, this function / functional interface will run.
-     * It's return value will be the same as the return value for the whole ifPassOrFail method.
+     * It's return value will be the same as the return value for the whole [ifPassOrFail] method.
      * @param failBlock If your business rules failed, this function / functional interface will run.
-     * This function will provide an AppException stored from a failing business result.
-     * It's return value will be the same as the return value for the whole ifPassOrFail method.
-     * @return A value that is returned from either the passBlock or the failBlock.
+     * This function will provide an [AppException] stored from a failing business result.
+     * It's return value will be the same as the return value for the whole [ifPassOrFail] method.
+     * @return A value that is returned from either the [passBlock] or the [failBlock].
      * */
     fun <V> ifPassOrFail(passBlock: () -> V, failBlock: (AppException) -> V) : V {
         return if (isPass) passBlock() else failBlock(appException!!)
