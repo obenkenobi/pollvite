2439ee3b688a02211f007f490cf01abeda2a3823
oren <orenbm11@gmail.com>
2023-06-12 00:26:55 -0400

    Document GrpcErrorHandler

--- projects/libraries/shared-errors/src/main/kotlin/com/pollvite/shared/errors/handlers.kt
+++ projects/libraries/shared-errors/src/main/kotlin/com/pollvite/shared/errors/handlers.kt
@@ -3,10 +3,18 @@ package com.pollvite.shared.errors
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * A static Grpc error handler
+ * */
 object GrpcErrorHandler {
     @JvmStatic
     private val log: Logger = LoggerFactory.getLogger(GrpcErrorHandler::class.java)
 
+    /**
+     * Handles an [Exception].
+     * @param e [Exception] to be handled.
+     * @return The appropriate [io.grpc.Status] for the provided [Exception].
+     * */
     fun handle(e: Exception): io.grpc.Status {
         log.debug("Handling Exception from GRPC Advice", e)
         return when(e) {
