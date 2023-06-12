13c973b5d8718143e3f0fcc28f6beff22b50d86b
oren <orenbm11@gmail.com>
2023-06-11 23:49:26 -0400

    Document BusinessRulesService

--- projects/libraries/springboot-businessrules-manager/src/main/kotlin/com/pollvite/springboot/businessrules/services/BusinessRulesService.kt
+++ projects/libraries/springboot-businessrules-manager/src/main/kotlin/com/pollvite/springboot/businessrules/services/BusinessRulesService.kt
@@ -6,17 +6,31 @@ import com.pollvite.springboot.businessrules.BusinessRuleResult
 import org.apache.logging.log4j.util.Strings
 import org.springframework.stereotype.Service
 
+/**
+ * A service that handles business rules.
+ * */
 interface BusinessRulesService {
-    fun processBrResults(violations: List<String>): BusinessRuleResult
+    /**
+     * Process business rule results, in the form of a list of [violations].
+     * If the list of [violations] is empty, a passing [BusinessRuleResult] is returned.
+     * Otherwise, if the list of [violations] is not empty, a failing [BusinessRuleResult] is returned.
+     * The [AppException] in the failing [BusinessRuleResult]
+     * contains a message of violation messages joined by a [separator].
+     * @param violations A list of violation messages from validating if your business rules have passed.
+     * @param separator A char separator that is used when joining messages in [violations]
+     * into a single message [String] to be added as a message in the [AppException]
+     * inside the failing [BusinessRuleResult] return value.
+     */
+    fun processBrResults(violations: List<String>, separator: Char = ';'): BusinessRuleResult
 }
 
 @Service
 class BusinessRulesServiceImpl : BusinessRulesService {
-    override fun processBrResults(violations: List<String>): BusinessRuleResult {
+    override fun processBrResults(violations: List<String>, separator: Char): BusinessRuleResult {
         return if (violations.isEmpty())
             BusinessRuleResult.pass()
         else {
-            val ex = AppException(ErrorStatus.BR_VIOLATION, Strings.join(violations, ';'))
+            val ex = AppException(ErrorStatus.BR_VIOLATION, Strings.join(violations, separator))
             BusinessRuleResult.fail(ex)
         }
     }
