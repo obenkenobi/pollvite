f762fbb99264f0de5656fb0c4071730113a1347b
oren <orenbm11@gmail.com>
2023-06-14 20:46:46 -0400

    Successfully enabled jsp and disabled loom

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/configuration/VirtualThreadConfig.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/configuration/VirtualThreadConfig.kt
@@ -1,25 +1,26 @@
 package com.pollvite.polledgeservice.configuration
 
-import org.apache.coyote.ProtocolHandler
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer
-import org.springframework.context.annotation.Bean
+//import org.apache.coyote.ProtocolHandler
+//import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
+//import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer
+//import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.task.AsyncTaskExecutor
-import org.springframework.core.task.support.TaskExecutorAdapter
-import java.util.concurrent.Executors
+//import org.springframework.core.task.AsyncTaskExecutor
+//import org.springframework.core.task.support.TaskExecutorAdapter
+//import java.util.concurrent.Executors
 
+// Todo: Enable virtual threads when moving Java 21
 @Configuration
 class VirtualThreadConfig {
-    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
-    fun asyncTaskExecutor(): AsyncTaskExecutor? {
-        return TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor())
-    }
-
-    @Bean
-    fun protocolHandlerVirtualThreadExecutorCustomizer(): TomcatProtocolHandlerCustomizer<*>? {
-        return TomcatProtocolHandlerCustomizer<ProtocolHandler> { protocolHandler: ProtocolHandler ->
-            protocolHandler.executor = Executors.newVirtualThreadPerTaskExecutor()
-        }
-    }
+//    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+//    fun asyncTaskExecutor(): AsyncTaskExecutor? {
+//        return TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor())
+//    }
+//
+//    @Bean
+//    fun protocolHandlerVirtualThreadExecutorCustomizer(): TomcatProtocolHandlerCustomizer<*>? {
+//        return TomcatProtocolHandlerCustomizer<ProtocolHandler> { protocolHandler: ProtocolHandler ->
+//            protocolHandler.executor = Executors.newVirtualThreadPerTaskExecutor()
+//        }
+//    }
 }
