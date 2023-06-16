2e85a1f991dc7ddf615b6f38a9244b36fab60e83
oren <orenbm11@gmail.com>
2023-06-16 13:18:25 -0400

    Move back to freemarker templates

--- projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/mvc/CustomErrorController.kt
+++ projects/services/pollvite-edge-service/src/main/kotlin/com/pollvite/polledgeservice/controllers/mvc/CustomErrorController.kt
@@ -1,15 +1,29 @@
 package com.pollvite.polledgeservice.controllers.mvc
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
+
 
 @Controller
 @RequestMapping("/error")
 class CustomErrorController: ErrorController {
 
     @RequestMapping
-    fun handleError(): String {
-        return "error"
+    fun handleError(httpRequest: HttpServletRequest): ModelAndView {
+        val errorPage = ModelAndView("error")
+        val status = getErrorCode(httpRequest)
+        val message = HttpStatus.valueOf(status).reasonPhrase
+        errorPage.addObject("status", status)
+        errorPage.addObject("message", message)
+        return errorPage
+    }
+
+    private fun getErrorCode(httpRequest: HttpServletRequest): Int {
+        return httpRequest
+            .getAttribute("jakarta.servlet.error.status_code") as Int
     }
 }
