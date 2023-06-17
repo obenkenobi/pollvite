b2d80adc4434a5c51c13defa350b597275229243
oren <orenbm11@gmail.com>
2023-06-17 03:37:43 -0400

    Update AppExceptionTest

--- projects/libraries/shared-errors/src/test/java/com/pollvite/shared/errors/AppExceptionTest.kt
+++ projects/libraries/shared-errors/src/test/java/com/pollvite/shared/errors/AppExceptionTest.kt
@@ -8,6 +8,7 @@ class AppExceptionTest {
     @Test
     fun testWithDefaultMessageToGrpc() {
         val ex = AppException(ErrorStatus.BUSINESS_RULES_VIOLATION)
+        assertEquals(io.grpc.Status.INVALID_ARGUMENT.code, ex.asGrpcStatus().code)
         assertEquals(ErrorStatus.BUSINESS_RULES_VIOLATION.description, ex.asGrpcStatus().description)
     }
 
@@ -15,6 +16,7 @@ class AppExceptionTest {
     fun testWithCustomMessageToGrpc() {
         val customMsg = "Custom Message"
         val ex = AppException(ErrorStatus.BUSINESS_RULES_VIOLATION, customMsg)
+        assertEquals(io.grpc.Status.INVALID_ARGUMENT.code, ex.asGrpcStatus().code)
         assertEquals(customMsg, ex.asGrpcStatus().description)
     }
 }
