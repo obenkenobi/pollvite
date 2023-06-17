676911c6ca46e270ef81eb218e43a212731d4e92
oren <orenbm11@gmail.com>
2023-06-17 01:09:46 -0400

    Add simple user service

---
+++ projects/services/user-service/src/test/kotlin/com/pollvite/pollrestservice/PollViteEdgeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pollvite.pollrestservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PollViteEdgeServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
