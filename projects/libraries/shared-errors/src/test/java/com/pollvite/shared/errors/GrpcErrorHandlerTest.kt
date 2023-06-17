84ec2d6f2577e9bd16cba77d1e2cdf513becd84f
oren <orenbm11@gmail.com>
2023-06-17 03:34:29 -0400

    Add testHandleAppException

--- projects/libraries/shared-errors/src/test/java/com/pollvite/shared/errors/GrpcErrorHandlerTest.kt
+++ projects/libraries/shared-errors/src/test/java/com/pollvite/shared/errors/GrpcErrorHandlerTest.kt
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test
 
 class GrpcErrorHandlerTest {
     @Test
-    fun testBaseException() {
+    fun testHandleBaseException() {
         val ex = Exception("")
         val status = GrpcErrorHandler.handle(ex)
         assertEquals(io.grpc.Status.INTERNAL.code, status.code)
+    }
+
+    @Test
+    fun testHandleAppException() {
+        val ex = AppException(ErrorStatus.BUSINESS_RULES_VIOLATION)
+        val status = GrpcErrorHandler.handle(ex)
+        assertEquals(io.grpc.Status.INVALID_ARGUMENT.code, status.code)
     }
 }
