d6ae49132f136cb78d7a2f38921acb1317342f64
oren <orenbm11@gmail.com>
2023-06-22 23:34:29 -0400

    Add unimplemented user service BLL layer

---
+++ projects/services/user-service/src/main/kotlin/com/pollvite/userservice/grpc/services/UserServiceGrpcImpl.kt
@@ -0,0 +1,40 @@
+package com.pollvite.userservice.grpc.services
+
+import com.pollvite.grpc.shared.IdPb
+import com.pollvite.grpc.user.*
+import com.pollvite.userservice.services.UserService
+import net.devh.boot.grpc.server.service.GrpcService
+import org.springframework.beans.factory.annotation.Autowired
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@GrpcService
+class UserServiceGrpcImpl(@Autowired val userService: UserService) : ReactorUserServiceGrpc.UserServiceImplBase() {
+
+    override fun getUserProfileByUUID(request: Mono<IdPb>): Mono<UserProfileFullPb> {
+        return request.map { userService.getUserProfileByUUID(it.value) }
+    }
+
+    override fun getUserProfileByPublicId(request: Mono<IdPb>): Mono<UserProfilePublicPb> {
+        return request.map { userService.getUserProfileByPublicId(it.value) }
+    }
+
+    override fun getUserProfilesByPublicIds(request: Flux<IdPb>): Flux<UserProfilePublicPb> {
+        return request.map { it.value }
+            .collectList()
+            .map { userService.getUserProfileByPublicIds(it) }
+            .flatMapIterable { it }
+    }
+
+    override fun createUserProfile(request: Mono<UserProfileCreatePb>): Mono<UserProfileFullPb> {
+        return request.map { userService.createUserProfile(it) }
+    }
+
+    override fun updateUserProfile(request: Mono<UserProfileUpdatePb>): Mono<UserProfileFullPb> {
+        return request.map { userService.updateUserProfile(it) }
+    }
+
+    override fun beginDeleteUserByUUID(request: Mono<IdPb>): Mono<UserProfileFullPb> {
+        return request.map { userService.beginDeleteUserByUUID(it.value) }
+    }
+}
---
+++ projects/services/user-service/src/main/kotlin/com/pollvite/userservice/services/userService.kt
@@ -0,0 +1,44 @@
+package com.pollvite.userservice.services
+
+import com.pollvite.grpc.user.UserProfileCreatePb
+import com.pollvite.grpc.user.UserProfileFullPb
+import com.pollvite.grpc.user.UserProfilePublicPb
+import com.pollvite.grpc.user.UserProfileUpdatePb
+import org.springframework.stereotype.Service
+
+interface UserService {
+    fun getUserProfileByUUID(uuid: String): UserProfileFullPb
+    fun getUserProfileByPublicId(publicId: String): UserProfilePublicPb
+    fun getUserProfileByPublicIds(publicIds: List<String>): List<UserProfilePublicPb>
+    fun createUserProfile(request: UserProfileCreatePb): UserProfileFullPb
+    fun updateUserProfile(request: UserProfileUpdatePb): UserProfileFullPb
+    fun beginDeleteUserByUUID(uuid: String): UserProfileFullPb
+}
+
+@Service
+class UserServiceImpl: UserService {
+    override fun getUserProfileByUUID(uuid: String): UserProfileFullPb {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserProfileByPublicId(publicId: String): UserProfilePublicPb {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUserProfileByPublicIds(publicIds: List<String>): List<UserProfilePublicPb> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createUserProfile(request: UserProfileCreatePb): UserProfileFullPb {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateUserProfile(request: UserProfileUpdatePb): UserProfileFullPb {
+        TODO("Not yet implemented")
+    }
+
+    override fun beginDeleteUserByUUID(uuid: String): UserProfileFullPb {
+        TODO("Not yet implemented")
+    }
+
+}
